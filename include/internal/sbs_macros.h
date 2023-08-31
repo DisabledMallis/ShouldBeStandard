5886f32e0bbdc34069c806c5322f9f9c1a71d8c6
Mike <mallisiscool@gmail.com>
2023-08-31 14:24:53 -0400

    Add sbs::breakpoint

---
+++ include/internal/sbs_macros.h
@@ -0,0 +1,7 @@
+#ifndef SBS_INTERNAL_MACROS
+#define SBS_INTERNAL_MACROS
+
+#define _BEGIN_SBS namespace sbs {
+#define _END_SBS }
+
+#endif
---
+++ tests/test_debugging.cpp
@@ -0,0 +1,6 @@
+#include <debugging>
+
+int main()
+{
+    sbs::breakpoint();
+}
